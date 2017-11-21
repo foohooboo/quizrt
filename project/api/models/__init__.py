cc364d11a05d7e5ceae95321ff5046fcdf6f783f
Andrew Aposhian <andrewsaposhian@gmail.com>
2017-11-21 15:45:29 -0700

    changed order of model imports

--- project/api/models/__init__.py
+++ project/api/models/__init__.py
@@ -1,19 +1,20 @@
 from .Users import User
 from .ClassProfile import ClassProfile
+from .QuizResult import QuizResult
 from .Quiz import Quiz
 from .Question import Question
 from .Answer import Answer
 from .Response import Response
-from .QuizResult import QuizResult
+
 from .QuizSession import QuizSession
 
 __all__ = [
     User,
     ClassProfile,
+    QuizResult,
     Quiz,
     Question,
     Answer,
     Response,
-    QuizResult,
     QuizSession,
 ]
