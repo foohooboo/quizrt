e68e6ff328db9a365ff723f4903d84a3f26bc0f3
Andrew Aposhian <andrewsaposhian@gmail.com>
2017-11-21 15:32:06 -0700

    added relation on response

--- project/api/models/Response.py
+++ project/api/models/Response.py
@@ -2,13 +2,14 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-from . import Question, Answer, User
+from . import Question, Answer, User, QuizResult
 
 class Response(models.Model):
     # user will be null for anonymous users
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
+    quiz_result = models.ForeignKey(QuizResult, on_delete=models.CASCADE)
 
     def __str__(self):
         return "Response for " + self.question.__str__()
@@ -16,4 +17,3 @@ class Response(models.Model):
     @property
     def quiz(self):
         return self.question.quiz
-    
