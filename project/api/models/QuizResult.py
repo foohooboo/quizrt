0bc8f5264d49fdc3b3d044e5b2bafdc77634fc10
Andrew Aposhian <andrewsaposhian@gmail.com>
2017-11-21 15:39:43 -0700

    added date field to quiz result model

--- project/api/models/QuizResult.py
+++ project/api/models/QuizResult.py
@@ -4,5 +4,7 @@ from django.db import models
 
 class QuizResult(models.Model):
     # Contains Responses which reference the QuizResult
+    date = models.DateField(required=False, null=True)
+
     def __str__(self):
         return "QuizResult"
