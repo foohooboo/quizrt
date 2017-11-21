1b8804d9efd88c33ba075d28942ff1eeb3236cd0
Andrew Aposhian <andrewsaposhian@gmail.com>
2017-11-21 15:42:07 -0700

    fixed mistake

--- project/api/models/QuizResult.py
+++ project/api/models/QuizResult.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class QuizResult(models.Model):
     # Contains Responses which reference the QuizResult
-    date = models.DateField(required=False, null=True)
+    date = models.DateField(blank=True, null=True)
 
     def __str__(self):
         return "QuizResult"
